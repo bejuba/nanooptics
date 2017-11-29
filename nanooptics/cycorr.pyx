f0bda2f60fcd43cb823a4858cb446a221d735af2
Bernd Sontheimer <bernd.sontheimer@posteo.de>
2017-11-29 13:53:53 +0100

    changed some types from int to uint64 to hopefully fix overflows and warnings in windows
    and added syncdiff method to calculate the timedifferences with respect to a specific sync channel

--- nanooptics/cycorr.pyx
+++ nanooptics/cycorr.pyx
@@ -57,7 +57,7 @@ cdef optcorr(_np.ndarray[_np.uint8_t, ndim=1] channel,
     cdef _np.uint64_t last_t2_index = len(timestamp) - 1
     cdef int i, j
     cdef _np.uint64_t tau
-    cdef _np.uint64_t g2_unnormalized = _np.zeros(2 * cutofftime, dtype=_np.int32)
+    cdef _np.uint64_t[:] g2_unnormalized = _np.zeros(2 * cutofftime, dtype=_np.int64)
 
     for i in range(last_t2_index):
         if channel[i] == chan0:
@@ -69,7 +69,7 @@ cdef optcorr(_np.ndarray[_np.uint8_t, ndim=1] channel,
                     g2_unnormalized[tau+cutofftime] += 1
         elif channel[i] == chan1:
             for j in range(i+1, last_t2_index):
-                tau = (timestamp[j] - timestamp[i])
+                tau = timestamp[j] - timestamp[i]
                 if tau > cutofftime:
                     break
                 if channel[j] == chan0:
@@ -146,7 +146,7 @@ cdef optcorr3(_np.ndarray[_np.uint8_t, ndim=1] channel,
              _np.int_t chan2_max):
     cdef _np.uint64_t last_t2_index = len(timestamp) - 1
     cdef _np.uint_t i, j, k, tau1, tau2
-    cdef g2 = _np.zeros((chan1_max - chan1_min, chan2_max - chan2_min), dtype=_np.int32)
+    cdef g2 = _np.zeros((chan1_max - chan1_min, chan2_max - chan2_min), dtype=_np.int)
 
     for i in range(last_t2_index):
         if channel[i] == chan0:
@@ -164,119 +164,122 @@ cdef optcorr3(_np.ndarray[_np.uint8_t, ndim=1] channel,
     return g2
 
 
+#
+# def timefilter(channel, timestamp, cutoff, resolution, syncchan,
+#                chan1,
+#                chan1_min,
+#                chan1_max,
+#                chan2,
+#                chan2_min,
+#                chan2_max):
+#     if channel.dtype != 'uint8':
+#         channel = _np.uint8(channel)
+#     cutoff = _np.uint64(cutoff / resolution)
+#     timestamp = _np.uint64(timestamp / resolution)
+#     chan, tt = cytimefilter(channel,
+#                           timestamp,
+#                             cutoff,
+#                           syncchan,
+#                           chan1,
+#                           chan1_min,
+#                           chan1_max,
+#                           chan2,
+#                           chan2_min,
+#                           chan2_max)
+#     return chan, tt * resolution
 
-def timefilter(channel, timestamp, cutoff, resolution, syncchan,
-               chan1,
-               chan1_min,
-               chan1_max,
-               chan2,
-               chan2_min,
-               chan2_max):
-    if channel.dtype != 'uint8':
+
+# @cython.cdivision
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+# cdef cytimefilter(_np.ndarray[_np.uint8_t, ndim=1] channel,
+#              _np.ndarray[_np.uint64_t, ndim=1] timestamp,
+#              _np.int_t cutoff,
+#              _np.int_t sync,
+#              _np.int_t chan1,
+#              _np.int_t chan1_min,
+#              _np.int_t chan1_max,
+#              _np.int_t chan2,
+#              _np.int_t chan2_min,
+#              _np.int_t chan2_max):
+#     cdef _np.uint64_t last_t2_index = len(timestamp) - 1
+#     cdef _np.uint_t i, j, tau
+#
+#     for i in range(last_t2_index):
+#         if channel[i] == sync:
+#             for j in range(i+1, last_t2_index):
+#                 if channel[j] == chan1:
+#                     tau = timestamp[j] - timestamp[i]
+#                     if tau > cutoff:
+#                          break
+#                     if (tau > chan1_min) or (tau < chan1_max):
+#                         timestamp[j] = 0
+#                 elif channel[j] == chan2:
+#                     tau = timestamp[j] - timestamp[i]
+#                     if tau > cutoff:
+#                          break
+#                     if (tau > chan2_min) or (tau < chan2_max):
+#                         timestamp[j] = 0
+#
+#         elif channel[i] == chan1:
+#             for j in range(i+1, last_t2_index):
+#                 if channel[j] == sync:
+#                     tau = timestamp[i] - timestamp[j]
+#                     if tau < cutoff:
+#                          break
+#                     if (tau > chan1_min) or (tau < chan1_max):
+#                         timestamp[i] = 0
+#
+#         elif channel[i] == chan2:
+#             for j in range(i+1, last_t2_index):
+#                 if channel[j] == sync:
+#                     tau = timestamp[i] - timestamp[j]
+#                     if tau < cutoff:
+#                          break
+#                     if (tau > chan2_min) or (tau < chan2_max):
+#                         timestamp[i] = 0
+#
+#
+#     mask = timestamp != 0
+#     return channel[mask], timestamp[mask]
+
+
+def syncdiff(channel, timestamp, syncchan, reverse=False):
+    if channel.dtype != _np.uint8:
         channel = _np.uint8(channel)
-    cutoff = _np.uint64(cutoff / resolution)
-    timestamp = _np.uint64(timestamp / resolution)
-    chan, tt = cytimefilter(channel,
-                          timestamp,
-                            cutoff,
-                          syncchan,
-                          chan1,
-                          chan1_min,
-                          chan1_max,
-                          chan2,
-                          chan2_min,
-                          chan2_max)
-    return chan, tt * resolution
+        print('internally converted channel to uint8 for cython')
+    if timestamp.dtype != _np.double:
+        timestamp = _np.double(timestamp)
+        print('internally converted timestamp to double for cython')
+
+    if reverse:
+        channel = channel[::-1]
+        timestamp = timestamp[::-1]
+
+    syncchan = _np.uint8(syncchan)
+    print('internally converted syncchan to uint8 for cython')
+
+    if reverse:
+        return _np.array(cysyncdiff(channel, timestamp, syncchan))[::-1]
+    else:
+        return _np.array(cysyncdiff(channel, timestamp, syncchan))
 
 
 @cython.cdivision
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef cytimefilter(_np.ndarray[_np.uint8_t, ndim=1] channel,
-             _np.ndarray[_np.uint64_t, ndim=1] timestamp,
-             _np.int_t cutoff,
-             _np.int_t sync,
-             _np.int_t chan1,
-             _np.int_t chan1_min,
-             _np.int_t chan1_max,
-             _np.int_t chan2,
-             _np.int_t chan2_min,
-             _np.int_t chan2_max):
-    cdef _np.uint64_t last_t2_index = len(timestamp) - 1
-    cdef _np.uint_t i, j, tau
+cdef cysyncdiff(_np.ndarray[_np.uint8_t, ndim=1] channel,
+                _np.ndarray[_np.double_t, ndim=1] timestamp,
+                _np.uint8_t syncchan):
+    cdef _np.uint64_t last_t2_index = len(timestamp)
+    cdef _np.int_t i
+    cdef _np.double_t lastsync = timestamp[0]
+    cdef _np.double_t[:] timediff = timestamp.copy()
 
     for i in range(last_t2_index):
-        if channel[i] == sync:
-            for j in range(i+1, last_t2_index):
-                if channel[j] == chan1:
-                    tau = timestamp[j] - timestamp[i]
-                    if tau > cutoff:
-                         break
-                    if (tau > chan1_min) or (tau < chan1_max):
-                        timestamp[j] = 0
-                elif channel[j] == chan2:
-                    tau = timestamp[j] - timestamp[i]
-                    if tau > cutoff:
-                         break
-                    if (tau > chan2_min) or (tau < chan2_max):
-                        timestamp[j] = 0
+        if channel[i] == syncchan:
+            lastsync = timestamp[i]
+        else:
+            timediff[i] -= lastsync
 
-        elif channel[i] == chan1:
-            for j in range(i+1, last_t2_index):
-                if channel[j] == sync:
-                    tau = timestamp[i] - timestamp[j]
-                    if tau < cutoff:
-                         break
-                    if (tau > chan1_min) or (tau < chan1_max):
-                        timestamp[i] = 0
-
-        elif channel[i] == chan2:
-            for j in range(i+1, last_t2_index):
-                if channel[j] == sync:
-                    tau = timestamp[i] - timestamp[j]
-                    if tau < cutoff:
-                         break
-                    if (tau > chan2_min) or (tau < chan2_max):
-                        timestamp[i] = 0
-
-
-    mask = timestamp != 0
-    return channel[mask], timestamp[mask]
-
-
-
-@cython.cdivision
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef cytimefilter(_np.ndarray[_np.uint8_t, ndim=1] channel,
-             _np.ndarray[_np.uint64_t, ndim=1] timestamp,
-             _np.int_t cutoff,
-             _np.int_t sync,
-             _np.int_t chan1,
-             _np.int_t chan1_min,
-             _np.int_t chan1_max,
-             _np.int_t chan2,
-             _np.int_t chan2_min,
-             _np.int_t chan2_max):
-    cdef _np.uint64_t last_t2_index = len(timestamp) - 1
-    cdef _np.uint_t i, j
-    cdef _np.uint64_t tau
-    cdef _np.uint64[:] timediff = timestamp
-
-    for i in range(last_t2_index):
-        if channel[i] == sync:
-            for j in range(i+1, last_t2_index):
-                if channel[j] == chan1:
-                    tau = timestamp[j] - timestamp[i]
-                    if tau > cutoff:
-                         break
-                    if (tau > chan1_min) or (tau < chan1_max):
-                        timestamp[j] = 0
-                elif channel[j] == chan2:
-                    tau = timestamp[j] - timestamp[i]
-                    if tau > cutoff:
-                         break
-                    if (tau > chan2_min) or (tau < chan2_max):
-                        timestamp[j] = 0
-    mask = timestamp != 0
-    return channel[mask], timestamp[mask]
+    return timediff
